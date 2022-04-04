4ab395fcf02634e34dfb0b76f11275470ec08243
IvanAtaefe <88833116+IvanAtaefe@users.noreply.github.com>
2022-04-04 20:42:27 -0300

    FirstScene

---
+++ Assets/Scripts/FirstScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int edad;
+    public Text Prueba;
+    public const int edadmaxima = 18;
+    void Start()
+    {
+        if (edad > 18)
+        {
+            Prueba.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Prueba.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+}
